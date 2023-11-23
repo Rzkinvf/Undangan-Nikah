ce03167743dadc2365dc74a3cd23a4fa03ab8139
Muhammad Rizki <fadilahmrizki433@gmail.com>
2023-11-23 13:36:26 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html",'homePage.html'],
+  theme: {
+    fontFamily: {
+      'Sacramento': ['Sacramento', 'cursive'],
+      'Josefin': ['Josefin Sans', 'sans-serif'],
+      'Arabic': ['Noto Naskh Arabic', 'serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
